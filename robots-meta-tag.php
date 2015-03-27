f0b00e8066112aa4f01f1527e70931691609f965
Gigaleet <David@gigaleet.com>
2015-03-27 15:19:09 -0400

    Add warning to the plug-in description

--- robots-meta-tag.php
+++ robots-meta-tag.php
@@ -2,7 +2,7 @@
 	/**
 	 * Plugin Name: Robots Meta Tag Manager
 	 * Plugin URI: https://github.com/Gigaleet/robots-meta-tag
-	 * Description: WordPress Robots Meta Tag Manager Plug-in, based on the robots meta tag tool built in to the CKG Blank theme (v2.1+).
+	 * Description: WordPress Robots Meta Tag Manager Plug-in, based on the robots meta tag tool built in to the CKG Blank theme (v2.1+).  <strong>Please DO NOT activate this plugin if you're running CKG Blank Theme version 2.1 or higher</strong>.
 	 * Version: 0.1.0 Beta
 	 * Author: David Lewis
 	 * Author URI: https://github.com/Gigaleet/
